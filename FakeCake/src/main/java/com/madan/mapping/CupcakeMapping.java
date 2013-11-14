48e3d185df5664f62462047838fc052c7f6c2a11
Madan Parameswaran <mparameswaran@citrisys.com>
2013-11-14 17:09:01 +0530

    CREATE: model and mapping classes for cupcake ADD: json java lib TODO: write actual mapping code

---
+++ FakeCake/src/main/java/com/madan/mapping/CupcakeMapping.java
@@ -0,0 +1,15 @@
+package com.madan.mapping;
+
+import com.madan.model.Cupcake;
+
+/**
+ * Created by Madan on 11/14/13.
+ */
+public class CupcakeMapping {
+
+    public static Cupcake parse(String json){
+
+        return null;
+    }
+
+}
---
+++ FakeCake/src/main/java/com/madan/model/Cupcake.java
@@ -0,0 +1,34 @@
+package com.madan.model;
+
+/**
+ * Created by Madan on 11/14/13.
+ */
+public class Cupcake {
+    private String name;
+    private String shortDescription;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
